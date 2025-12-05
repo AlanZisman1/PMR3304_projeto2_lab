db698162910a38f09e2f875016dd31ebb4780a4e
Alan Rosenthal Zisman <alanrosen2905@gmail.com>
2025-12-05 01:37:00 -0300

    refactor: passa a usar forms nas views

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import Http404
 from .models import Post
+from .forms import PostForm
 
 # Listagem
 def post_list(request):
@@ -15,26 +16,30 @@ def post_detail(request, pk):
         raise Http404("Post não encontrado")
     return render(request, 'blog/post_detail.html', {'post': post})
 
-# Criação
+# CRIAÇÃO (Com Form)
 def post_create(request):
     if request.method == 'POST':
-        # Pegando dados direto do dicionário POST
-        titulo = request.POST.get('title')
-        conteudo = request.POST.get('content')
-        # Criando o objeto sem validação
-        Post.objects.create(title=titulo, content=conteudo)
-        return redirect('post_list')
-    return render(request, 'blog/post_form.html')
+        form = PostForm(request.POST)
+        if form.is_valid(): # O Django verifica se os campos estão ok
+            form.save()
+            return redirect('post_list')
+    else:
+        form = PostForm() # Cria um formulário vazio
+    return render(request, 'blog/post_form.html', {'form': form})
 
-# Edição
+# EDIÇÃO (Com Form)
 def post_update(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == 'POST':
-        post.title = request.POST.get('title')
-        post.content = request.POST.get('content')
-        post.save()
-        return redirect('post_detail', pk=post.pk)
-    return render(request, 'blog/post_form.html', {'post': post})
+        # Passamos 'instance=post' para o Django saber QUAL post atualizar
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        # Preenche o formulário com os dados existentes do post
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_form.html', {'form': form})
 
 # Remoção
 def post_delete(request, pk):
